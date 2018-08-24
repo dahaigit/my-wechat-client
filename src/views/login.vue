32d86dc4b8e323a35e656f7d61422c4a9db26fad
menghailong <menghailong@xiyuemedia.com>
2018-08-24 13:58:01 +0800

    bug

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4aebe20c12f506a3&redirect_uri=http%3A%2F%2Fwechat.subprice.cn%2Flogin&response_type=code&scope=snsapi_userinfo&state=29fc17c55882acb025e24277fb603ffb&connect_redirect=1#wechat_redirect">登陆</a>
+    <a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4aebe20c12f506a3&redirect_uri=http%3A%2F%2Fwechat.subprice.cn%2F%23%2Fauth&response_type=code&scope=snsapi_userinfo&state=bc17befd6d5060f16de95e38f6eaf69c&connect_redirect=1#wechat_redirect">登陆</a>
   </div>
 </template>
 
