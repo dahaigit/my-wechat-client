28d99d3008eff05697bfdec3de028b82c97c617b
menghailong <menghailong@xiyuemedia.com>
2018-08-24 16:20:57 +0800

    去掉无用代码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,9 +1,3 @@
-<template>
-  <div>
-    <a href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4aebe20c12f506a3&redirect_uri=http%3A%2F%2Fwechat.subprice.cn%2F%23%2Flogin&response_type=code&scope=snsapi_userinfo&state=bc17befd6d5060f16de95e38f6eaf69c&connect_redirect=1#wechat_redirect">登陆</a>
-  </div>
-</template>
-
 <script>
   export default {
     name: 'login',
